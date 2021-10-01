6a6c42f612e9d1d7fce042c7685e975590158b65
Sumin Baek <49911177+baeksumin@users.noreply.github.com>
2021-10-01 10:06:28 +0900

    컬럼 추가 및 삭제

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import math
 
 # 데이터 불러오기
 data = pd.read_csv('/Users/baeksumin/apps/electricity/dataset/energy.csv', encoding = 'cp949')
@@ -15,3 +16,11 @@ data_sort = data.sort_values(by=["date", "num"], ascending=[True, True], ignore_
 train = data_sort.loc[:112319] # 20200601~20200817
 test = data_sort.loc[112320:] # 20200818~20200824
 
+# 필요한 컬럼 추가
+train['불쾌지수'] = (0.81 * train.loc[:, '기온(°C)'] + 0.01 * train['습도(%)'] * (0.99 * train['기온(°C)'] - 14.3) + 46.3).round(1)
+train['체감온도'] = (13.12 + 0.6215 * train['기온(°C)'] - 11.37 * train['풍속(m/s)'].apply(lambda x: math.pow(x,0.15)) + 0.3965 * train['풍속(m/s)'].apply(lambda x: math.pow(x, 0.15)) * train['기온(°C)']).round(1)
+
+# 필요하지 않은 컬럼 제거
+train_drop = train.drop(['강수량(mm)'], axis = 1)
+
+print(train_drop)
