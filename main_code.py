b23db6f079944a32ae0ff8fe772a3aad9f916b64
Sumin Baek <49911177+baeksumin@users.noreply.github.com>
2021-10-20 23:00:22 +0900

    공휴일데이터

--- main_code.py
+++ main_code.py
@@ -9,8 +9,10 @@ from sklearn.cluster import KMeans
 import requests
 # import datetime
 from bs4 import BeautifulSoup
+from prophet import Prophet
 
-pd.set_option('mode.chained_assignment',  None) # 경고 무시
+pd.set_option('mode.chained_assignment',  None) # 경고 무시 설정
+pd.set_option('display.max_columns', 100) # 데이터 프레임 전체 열 출력 설정
 
 # 데이터 불러오기
 data = pd.read_csv('/Users/baeksumin/apps/electricity/dataset/energy.csv', encoding = 'cp949')
@@ -52,107 +54,124 @@ train_drop = train_add.drop(['강수량(mm)'], axis = 1)
 # print(train_drop)
 
 # 시각화 - 건물별 전력사용량 패턴 알기
-fig = plt.figure(figsize = (15, 40))
-for num in train_drop['num'].unique():
-    df = train_drop[train_drop.num == num]
-    df = df.groupby(['weekday', 'time'])['전력사용량(kWh)'].mean().reset_index().pivot('weekday', 'time', '전력사용량(kWh)')
-    plt.subplot(12, 5, num)
-    sns.heatmap(df)
-    plt.title(f'building {num}')
-    plt.xlabel('')
-    plt.ylabel('')
-    plt.yticks([])
-    plt.savefig("/Users/baeksumin/apps/electricity/image/num_visualization.png")
+# fig = plt.figure(figsize = (15, 40))
+# for num in train_drop['num'].unique():
+#     df = train_drop[train_drop.num == num]
+#     df = df.groupby(['weekday', 'time'])['전력사용량(kWh)'].mean().reset_index().pivot('weekday', 'time', '전력사용량(kWh)')
+#     plt.subplot(12, 5, num)
+#     sns.heatmap(df)
+#     plt.title(f'building {num}')
+#     plt.xlabel('')
+#     plt.ylabel('')
+#     plt.yticks([])
+    # plt.savefig("/Users/baeksumin/apps/electricity/image/num_visualization.png")
 # 건물별로 비슷한 전력사용량 패턴을 보이는 것이 있다는 것을 알 수 있다.
 
 
-# # 군집화
-# # 건물을 기준으로 하는 data frame 생성
-# by_weekday = train_drop.groupby(['num','weekday'])['전력사용량(kWh)'].median().reset_index().pivot('num','weekday','전력사용량(kWh)').reset_index()
-# by_time = train_drop.groupby(['num','time'])['전력사용량(kWh)'].median().reset_index().pivot('num','time','전력사용량(kWh)').reset_index().drop('num', axis = 1)
-# df = pd.concat([by_weekday, by_time], axis= 1)
-# columns = ['num'] + ['day'+str(i) for i in range(7)] + ['time'+str(i) for i in range(24)]
-# df.columns = columns
-# # print(df)
+# 군집화
+# 건물을 기준으로 하는 data frame 생성
+by_weekday = train_drop.groupby(['num','weekday'])['전력사용량(kWh)'].median().reset_index().pivot('num','weekday','전력사용량(kWh)').reset_index()
+by_time = train_drop.groupby(['num','time'])['전력사용량(kWh)'].median().reset_index().pivot('num','time','전력사용량(kWh)').reset_index().drop('num', axis = 1)
+df = pd.concat([by_weekday, by_time], axis= 1)
+columns = ['num'] + ['day'+str(i) for i in range(7)] + ['time'+str(i) for i in range(24)]
+df.columns = columns
+# print(df)
 
-# # '전력사용량'이 아닌 '요일과 시간대에 따른 전력 사용량의 경향성'에 따라서만 군집화 할 것이므로, 특수한 scaling이 필요함
-# # standard scaling
-# for i in range(len(df)):
-#     # 요일 별 전력 중앙값에 대해 scaling
-#     df.iloc[i,1:8] = (df.iloc[i,1:8] - df.iloc[i,1:8].mean())/df.iloc[i,1:8].std()
-#     # 시간대별 전력 중앙값에 대해 scaling
-#     df.iloc[i,8:] = (df.iloc[i,8:] - df.iloc[i,8:].mean())/df.iloc[i,8:].std()
+# '전력사용량'이 아닌 '요일과 시간대에 따른 전력 사용량의 경향성'에 따라서만 군집화 할 것이므로, 특수한 scaling이 필요함
+# standard scaling
+for i in range(len(df)):
+    # 요일 별 전력 중앙값에 대해 scaling
+    df.iloc[i,1:8] = (df.iloc[i,1:8] - df.iloc[i,1:8].mean())/df.iloc[i,1:8].std()
+    # 시간대별 전력 중앙값에 대해 scaling
+    df.iloc[i,8:] = (df.iloc[i,8:] - df.iloc[i,8:].mean())/df.iloc[i,8:].std()
 
-# # 시각화
-# fig = plt.figure(figsize = (10,3))
-# # 요일별
-# for i in range(len(df)):
-#     plt.plot(df.iloc[i, 1:8], alpha = 0.5, linewidth = 0.5)
-# # plt.savefig("/Users/baeksumin/apps/electricity/image/clustering_week.png")
-# # 시간별
-# fig = plt.figure(figsize = (20,3))
-# for i in range(len(df)):
-#     plt.plot(df.iloc[i, 8:], alpha = 0.5, linewidth = 0.5)
-# # plt.savefig("/Users/baeksumin/apps/electricity/image/clustering_time.png")
+# 시각화
+fig = plt.figure(figsize = (10,3))
+# 요일별
+for i in range(len(df)):
+    plt.plot(df.iloc[i, 1:8], alpha = 0.5, linewidth = 0.5)
+# plt.savefig("/Users/baeksumin/apps/electricity/image/clustering_week.png")
+# 시간별
+fig = plt.figure(figsize = (20,3))
+for i in range(len(df)):
+    plt.plot(df.iloc[i, 8:], alpha = 0.5, linewidth = 0.5)
+# plt.savefig("/Users/baeksumin/apps/electricity/image/clustering_time.png")
 
-# # k-means clustering
-# # elbow method를 통해 군집의 개수 결정
-# def change_n_clusters(n_clusters, data):
-#     sum_of_squared_distance = []
-#     for n_cluster in n_clusters:
-#         kmeans = KMeans(n_clusters=n_cluster)
-#         kmeans.fit(data)
-#         sum_of_squared_distance.append(kmeans.inertia_)
+# k-means clustering
+# elbow method를 통해 군집의 개수 결정
+def change_n_clusters(n_clusters, data):
+    sum_of_squared_distance = []
+    for n_cluster in n_clusters:
+        kmeans = KMeans(n_clusters=n_cluster)
+        kmeans.fit(data)
+        sum_of_squared_distance.append(kmeans.inertia_)
         
-#     plt.figure(1 , figsize = (8, 5))
-#     plt.plot(n_clusters , sum_of_squared_distance , 'o')
-#     plt.plot(n_clusters , sum_of_squared_distance , '-' , alpha = 0.5)
-#     plt.xlabel('Number of Clusters')
-#     plt.ylabel('Inertia')
-#     # plt.savefig("/Users/baeksumin/apps/electricity/image/count_of_clusters.png")
+    plt.figure(1 , figsize = (8, 5))
+    plt.plot(n_clusters , sum_of_squared_distance , 'o')
+    plt.plot(n_clusters , sum_of_squared_distance , '-' , alpha = 0.5)
+    plt.xlabel('Number of Clusters')
+    plt.ylabel('Inertia')
+    # plt.savefig("/Users/baeksumin/apps/electricity/image/count_of_clusters.png")
 
-# change_n_clusters([2,3,4,5,6,7,8,9,10,11], df.iloc[:,1:])
+change_n_clusters([2,3,4,5,6,7,8,9,10,11], df.iloc[:,1:])
 
-# # 그래프 확인 결과 최적 군집 수는 4로 결정
+# 그래프 확인 결과 최적 군집 수는 4로 결정
 
+# prophet이 포맷으로 rename
+# print(train_drop)
 
-# # 휴일 데이터 가져오기 (공공데이터포털 API사용)
-# def print_whichday(year, month, day) :
-#     r = ['월요일', '화요일', '수요일', '목요일', '금요일', '토요일', '일요일']
-#     aday = dt.date(year, month, day)
-#     bday = aday.weekday()
-#     return r[bday]
+# 휴일 데이터 가져오기 (공공데이터포털 API사용)
+def print_whichday(year, month, day) :
+    r = ['월요일', '화요일', '수요일', '목요일', '금요일', '토요일', '일요일']
+    aday = dt.date(year, month, day)
+    bday = aday.weekday()
+    return r[bday]
 
-# def get_request_query(url, operation, params, serviceKey):
-#     import urllib.parse as urlparse
-#     params = urlparse.urlencode(params)
-#     request_query = url + '/' + operation + '?' + params + '&' + 'serviceKey' + '=' + serviceKey
-#     return request_query
+def get_request_query(url, operation, params, serviceKey):
+    import urllib.parse as urlparse
+    params = urlparse.urlencode(params)
+    request_query = url + '/' + operation + '?' + params + '&' + 'serviceKey' + '=' + serviceKey
+    return request_query
 
-# year = 2020
-# mykey = "VygvqzZz%2FxRZ%2Bp3i119xUZJ1i2EY%2FIrsCPR0Hgtdggi6ha%2FiL4F7oKwutUm26UkjD188qyIp8WZk70a1bGqdwg%3D%3D"
+year = 2020
+mykey = "VygvqzZz%2FxRZ%2Bp3i119xUZJ1i2EY%2FIrsCPR0Hgtdggi6ha%2FiL4F7oKwutUm26UkjD188qyIp8WZk70a1bGqdwg%3D%3D"
 
-# holiday_list = []
-# for month in range(6,9):
+date_name = []
+loc_date = []
+for month in range(6,9):
 
-#     month = '0' + str(month)
+    month = '0' + str(month)
     
-#     url = 'http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService'
-#     operation = 'getRestDeInfo'
-#     params = {'solYear':year, 'solMonth':month}
+    url = 'http://apis.data.go.kr/B090041/openapi/service/SpcdeInfoService'
+    operation = 'getRestDeInfo'
+    params = {'solYear':year, 'solMonth':month}
 
-#     request_query = get_request_query(url, operation, params, mykey)
-#     get_data = requests.get(request_query)    
+    request_query = get_request_query(url, operation, params, mykey)
+    get_data = requests.get(request_query)    
 
-#     if True == get_data.ok:
-#         soup = BeautifulSoup(get_data.content, 'html.parser')        
+    if True == get_data.ok:
+        soup = BeautifulSoup(get_data.content, 'html.parser')        
         
-#         item = soup.findAll('item')
-#         #print(item);
+        item = soup.findAll('item')
+        #print(item);
 
-#         for i in item:
-#             day = int(i.locdate.string[-2:])
-#             weekname = print_whichday(int(year), int(month), day)
-#             holidate = str(i.locdate.string)
-#             holiday_list.append(holidate)
-# print(holiday_list)
+        for i in item:
+            day = int(i.locdate.string[-2:])
+            weekname = print_whichday(int(year), int(month), day)
+            locdate = str(i.locdate.string)
+            datename = str(i.datename.string)
+            loc_date.append(locdate)
+            date_name.append(datename)
+print(loc_date)
+print(date_name)            
+# 20200606, 20200815, 20200817 공휴일 확인
+
+# prophet이 원하는 형태로 공휴일 데이터 가공
+def holidays_to_df():
+    holidays = pd.DataFrame({
+        'holiday' : date_name,
+        'ds' : loc_date,
+        'lower_window' : 0,
+        'upper_window' : 0
+    })
+    return holidays
